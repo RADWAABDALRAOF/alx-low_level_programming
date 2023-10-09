b12d9958b8e1801c7c6539e62055ae8589fac5d1
radwa <radwa@gmail.com>
2023-10-08 17:51:50 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- the entry point
+ *
+ * Description: programe to Put
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
