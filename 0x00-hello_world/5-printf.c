a1eb26216c35cd7b74acece4f264be498d5d0415
radwa <radwa@gmail.com>
2023-10-08 17:58:01 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- the entry point
+ *
+ * Description: Printf function
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
