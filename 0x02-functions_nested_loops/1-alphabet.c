49d603b9808cfa108878182b89c0cbe43492693a
radwa <radwa@gmail.com>
2023-10-29 04:30:26 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+* print_alphabet_x10  loop 10 tms
+* Description: function for alph
+* Return: void here
+*/
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar('c');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
